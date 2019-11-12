f0131a98a8fa6f3ead4c2ad54e36ccc5c28a6662
chenshouyu <chenshouyu@kuaishou.com>
2019-11-13 02:27:24 +0800

    update typescript template eslint config

--- template/typescript/.eslintrc.js
+++ template/typescript/.eslintrc.js
@@ -10,20 +10,37 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint'
+    'prettier/@typescript-eslint',
+    'prettier/react'
   ],
+  plugins: ['react-hooks'],
   parserOptions: {
     project: path.resolve(__dirname, './tsconfig.json'),
     tsconfigRootDir: __dirname,
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn'],
-    'no-console': 'error'
+    'no-console': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-unused-vars': ['warn', { args: 'none', ignoreRestSiblings: true }],
+    '@typescript-eslint/member-ordering': 'error',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'no-duplicate-imports': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-restricted-globals': ['error', 'name']
   }
 };
